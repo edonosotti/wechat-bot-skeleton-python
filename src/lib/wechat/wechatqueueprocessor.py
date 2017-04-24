16883b05066691ccd4f50897790473ad0b2eddff
Edoardo Nosotti <edoardo.nosotti@gmail.com>
2017-04-24 14:59:49 +0200

    Complete the queue processor.

--- src/lib/wechat/wechatqueueprocessor.py
+++ src/lib/wechat/wechatqueueprocessor.py
@@ -20,29 +20,34 @@ class WeChatQueueProcessor(object):
         )
 
         response = urllib.request.urlopen(url)
-        body = response.read().decode('utf-8')
-        token_info = json.loads(body)
+        response_body = response.read().decode('utf-8')
+        token_info = json.loads(response_body)
 
         if not 'errcode' in token_info:
-            logging.debug("WeChat access token is %s", token_info['access_token'])
             return token_info['access_token']
-        else:
-            logging.error("Error getting WeChat access token: %s", body)
+
+        logging.error("Error getting WeChat access token: %s", body)
         return None
 
     # See: http://admin.wechat.com/wiki/index.php?title=Customer_Service_Messages
     def process_message(self, message):
         access_token = self.get_access_token()
+
         if access_token != None:
             url = "https://api.wechat.com/cgi-bin/message/custom/send?access_token=%s" % access_token
-            body = {
+            request_body = {
                 'touser': message['sender'],
                 'msgtype': 'text',
                 'text': {
-                    'content': 'Hello World'
+                    'content': 'Your request has been processed, thank you!'
                 }
             }
-            # TODO - send a reply
-            # response = urllib.request.urlopen(url, body)
-            # body = response.read().decode('utf-8')
-            # print(body)
+
+            response = urllib.request.urlopen(url, json.dumps(request_body).encode('utf-8'))
+            response_body = response.read().decode('utf-8')
+
+            if 'errcode' in response_body and response_body['errcode'] == 0:
+                logging.error("Error sending Customer Service type message: %s", response_body)
+                return True
+
+        return False
